3e0088afab74379933bdc65036423f8647eded19
Carlos Brathwaite <carlosbrathwaite.rad@gmail.com>
2025-12-02 01:10:11 +0500

    db seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,29 @@ class DatabaseSeeder extends Seeder
         // Seed roles first
         $this->call(RoleSeeder::class);
 
+        // Create admin users
+        User::create([
+            'full_name' => 'Super Admin',
+            'email' => 'superadmin@forevestor.com',
+            'phone' => '+1234567890',
+            'password' => bcrypt('.com2025'),
+            'role_id' => 1, // super-admin
+            'status' => 'active',
+            'terms_agreed' => true,
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'full_name' => 'Admin',
+            'email' => 'admin@forevestor.com',
+            'phone' => '+1234567891',
+            'password' => bcrypt('.com2025'),
+            'role_id' => 2, // admin
+            'status' => 'active',
+            'terms_agreed' => true,
+            'email_verified_at' => now(),
+        ]);
+
         // User::factory(10)->create();
 
         // User::factory()->create([
